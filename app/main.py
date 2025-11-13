23c4e8eb4806e6c7d52b507b44bc8a05a5e7a84d
Ivan Korvatko <vanyakorvatko@gmail.com>
2025-11-13 17:25:28 +0200

    changed (made linter formater)

--- app/main.py
+++ app/main.py
@@ -14,10 +14,6 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
     }
 def format_linter_report(linter_report: dict) -> list:
     return [
-        {
-            "errors": [format_linter_error(e) for e in errors],
-            "path": path,
-            "status": "failed" if errors else "passed"
-        }
+        format_single_linter_file(path, errors)
         for path, errors in linter_report.items()
     ]
